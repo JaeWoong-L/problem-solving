585a2f6bab507f002990c2524ea1c043d8f7d79f
JaeWoong-L <wodnd6646@gmail.com>
2021-08-22 20:52:22 +0900

    210822 - dp

--- 9252.cpp
+++ 9252.cpp
@@ -1,50 +1,49 @@
 /*************************************
  DP - Gold 5
  LCS 2
- * 전형적인 dp문제
- * dp배열의 시작지점이 0이 아닌 -1.
+ * 9251 문제에서 실제 LCS string을 찾는 문제.
+ * bottom-up으로 풀려다가 복잡하기만 하고 성과가 없었다.
+ * 재귀로 푸는 것이 훨씬 간단하고 명료함.
 *************************************/
 #include <iostream>
 #include <string>
 using namespace std;
 int dp[1001][1001] = {0};
+string s1, s2;
 string LCS = "";
+
+void find_lcs(int i, int j) {
+    if(dp[i][j] == 0) return;
+    if(s1[i-1] == s2[j-1]) {
+        LCS = s1[i-1] + LCS;
+        find_lcs(i-1, j-1);
+    }
+    else {
+        dp[i-1][j] > dp[i][j-1] ? find_lcs(i-1, j) : find_lcs(i, j-1);
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    string s1, s2;
     cin >> s1 >> s2;
     int len1 = s1.length();
     int len2 = s2.length();
 
-    int count = 1;
     for(int i = 1; i <= len1; i++) {
-        bool isFound = false;
         for(int j = 1; j <= len2; j++) {
             if(s1[i-1] == s2[j-1]) {
                 dp[i][j] = dp[i-1][j-1] + 1;
-                if(isFound == false && dp[i][j] == count) {
-                    LCS += s1[i-1];
-                    isFound = true;
-                    count++;
-                }
             }
             else {
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         }
     }
-    cout << LCS.length() << endl;
 
-    for(int i = 0; i <= len1; i++) {
-        for(int j = 0; j <= len2; j++) {
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
-    }
+    find_lcs(len1, len2);
 
     int answer = dp[len1][len2];
     cout << answer << endl;
