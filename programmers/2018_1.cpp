33f6d0a0e3972181c7734227c8036cd6c2c524e5
JaeWoong-L <wodnd6646@gmail.com>
2021-08-28 01:00:46 +0900

    2018 카카오1차 - 비트연산

---
+++ programmers/2018_1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    vector<int> arr_sum(n);
+    
+    for(int i = 0; i < n; i++) {
+        arr_sum[i] = arr1[i] | arr2[i];
+        string line = "";
+        for(int j = 0; j < n; j++) {
+            if(arr_sum[i] % 2 == 0) {
+                line = " " + line;
+            }
+            else {
+                line = "#" + line;
+            }
+            arr_sum[i] = arr_sum[i] >> 1;
+        }
+        answer.push_back(line);
+    }
+    
+    return answer;
+}
+
+int main() {
+    int n = 5;
+    vector<int> arr1 = {9, 20, 28, 18, 11};
+    vector<int> arr2 = {30, 1, 21, 17, 28};
+    vector<string> ans = solution(n, arr1, arr2);
+    for(int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << endl;
+    }
+
+    return 0;
+}
