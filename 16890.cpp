ba52b9888c3f49f0fb6c5e82e26f2ee516cad1dd
JaeWoong-L <wodnd6646@gmail.com>
2021-09-06 01:26:07 +0900

    문자열, deque - 피드백 추가

--- 16890.cpp
+++ 16890.cpp
@@ -1,59 +1,61 @@
 /*************************************
  문자열, deque - Gold 3
  창업
- * string만으로 문제를 풀었지만, 이런 경우엔 deque을 사용하는 것이 바람직하다.
+ * 1. 처음엔 string으로 풀었으나,
+ * input string의 양끝에서만 삭제가 발생하므로 deque을 사용하는 것이 바람직하다.
+ * 
+ * 2. 문자열의 앞뒤 모두로 문자가 추가되는 경우,
+ * 문자열 2개를 만든 뒤 이어붙이면 훨씬 간편하다.
+ * (앞뒤 인덱스를 따로 만들어서 값을 증가,감소시키는 시간을 절약할 수 있다.)
+ * 
+ * 3. 반복문이 필요할 때 주로 while문을 사용하는 경향이 있는데,
+ * for문을 사용하는 것이 가독성이 좋은 경우가 많은 것 같다.
+ * 
+ * 참고: https://yeeybook.tistory.com/136
 *************************************/ 
 #include <iostream>
 #include <string>
+#include <deque>
 #include <algorithm>
 
 using namespace std;
 
-bool desc(char &a, char &b) {
-    return a > b;
-}
-
 string solution(string s1, string s2) {
-    string answer;
-    int size = s1.size();
-    for(int i = 0; i < size; i++) {
-        answer += "?";
-    }
+    deque<char> koo, cube;
+    int n = s1.size();
+    string answer(n, '?'); // 정답 문자열 초기화
 
-    sort(s1.begin(), s1.end());
-    sort(s2.begin(), s2.end(), desc);
+    // 정렬
+    sort(s1.begin(), s1.end()); // 오름차순
+    sort(s2.begin(), s2.end(), greater<char>()); // 내림차순
     
-    if(size % 2 == 0) {
-        s1 = s1.substr(0, size/2);
-        s2 = s2.substr(0, size/2);
+    for(int i = 0; i < (n + 1) / 2; i++) {
+        koo.push_back(s1[i]);
     }
-    else {
-        s1 = s1.substr(0, size/2 + 1);
-        s2 = s2.substr(0, size/2);
+    for(int i = 0; i < n / 2; i++) {
+        cube.push_back(s2[i]);
     }
     
-    int idx_left = 0, idx_right = size - 1;
-    while(s1.size() > 0 || s2.size() > 0) {
-        if(s1.front() < s2.front()) {
-            answer[idx_left++] = s1.front();
-            s1.erase(0, 1);
+    int idx_left = 0, idx_right = n - 1;
+    while(koo.size() > 0 || cube.size() > 0) {
+        if(koo.front() < cube.front()) {
+            answer[idx_left++] = koo.front();
+            koo.pop_front();
         }
         else {
-            answer[idx_right--] = s1.back();
-            s1.erase(s1.size() - 1, 1);     
+            answer[idx_right--] = koo.back();
+            koo.pop_back();     
         }
-        // cout << "1: " << answer << endl;
-        if(s2.size() == 0) break;
+        if(koo.size() == 0) break;
 
-        if(s2.front() > s1.front()) {
-            answer[idx_left++] = s2.front();
-            s2.erase(0, 1);
+        if(cube.front() > koo.front()) {
+            answer[idx_left++] = cube.front();
+            cube.pop_front();
         }
         else {
-            answer[idx_right--] = s2.back();
-            s2.erase(s2.size() - 1, 1);
+            answer[idx_right--] = cube.back();
+            cube.pop_back();
         }
-        // cout << "2: " << answer << endl;
     }
 
     return answer;
